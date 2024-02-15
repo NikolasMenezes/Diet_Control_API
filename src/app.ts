8b31dbbd9df1c23d9758fc8cea516db77c46be6d
Nikolas Menezes <nikolasdev.contato@gmail.com>
2024-02-15 17:56:01 -0300

    feat: adding cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 
 const app = express();
 
 import router from './router'
 
+app.use(cors())
 app.use(express.json())
 app.use('/api', router)
 
